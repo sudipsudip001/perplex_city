b393005c4baa897542e9bcd5f12a92f50d61d9f4
sudipsudip001 <sudipshrestha2051219@gmail.com>
2026-05-13 15:47:52 +0545

    modify questioning and create better project structure

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-
+from dotenv import load_dotenv
 from app.api.routes import search
 
 app: FastAPI = FastAPI()
 
 app.include_router(search.router)
 
+load_dotenv()
 
 @app.get("/")  # type: ignore[misc]
 def get_health() -> dict[str, str]:
